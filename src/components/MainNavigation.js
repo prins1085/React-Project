384d78a7586dcb249ad6d558e88abe092bed44af
Prins Harkhani <harkhaniprins2001@gmail.com>
2023-04-26 12:05:46 +0530

    Modified Logout Button

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -4,38 +4,46 @@ import LogoutPopUp from "./UI/LogoutPopUp";
 
 const MainNavigation = () => {
   const [logoutPopUpOpen, setLogoutPopUpOpen] = useState(false);
+  const [logoutDropdownOpen, setLogoutDropdownOpen] = useState(false);
   const storedData = localStorage.getItem("login_data");
   const parsedData = JSON.parse(storedData);
   const role = parsedData?.role;
 
   const navigate = useNavigate();
 
-
   const logoutHandler = () => {
     setLogoutPopUpOpen(true);
-   
-  }
+    setLogoutDropdownOpen(false);
+  };
 
   const LogoutAction = () => {
-    localStorage.removeItem('login_data');
+    localStorage.removeItem("login_data");
     setLogoutPopUpOpen(false);
-    navigate('/login');
-
-  }
+    navigate("/login");
+  };
 
   const closeLogoutPopUp = () => {
     setLogoutPopUpOpen(false);
-  }
+  };
+
+  const toggleLogout = () => {
+    setLogoutDropdownOpen(!logoutDropdownOpen);
+  };
 
   return (
     <Fragment>
-      {logoutPopUpOpen && <LogoutPopUp onCancel={closeLogoutPopUp} onConfirm={LogoutAction}/>}
+      {logoutPopUpOpen && (
+        <LogoutPopUp onCancel={closeLogoutPopUp} onConfirm={LogoutAction} />
+      )}
       <header className="flex justify-between bg-gray-300 py-4 md:px-6">
         <div className="text-xl font-bold">
           <Link to="">Project Name</Link>
         </div>
         <div className="flex gap-7">
-          <Link to="/explore-post" className="text-gray-700 hover:text-gray-900">
+          <Link
+            to="/explore-post"
+            className="text-gray-700 hover:text-gray-900"
+          >
             Explore Post
           </Link>
           {role === "admin" && (
@@ -48,13 +56,28 @@ const MainNavigation = () => {
           )}
         </div>
 
-        <div>
+        <div className="relative">
           {role ? (
-           <button onClick={logoutHandler}>Logout</button>
+            <img
+              src="https://freesvg.org/img/abstract-user-flat-4.png"
+              alt="profile icon"
+              className="w-8 h-8 bg-transparent cursor-pointer"
+              onClick={toggleLogout}
+            />
           ) : (
             <Link to="/login" className="text-gray-700 hover:text-gray-900">
               Login
             </Link>
+          )}
+          {logoutDropdownOpen && (
+            <div className="absolute mt-2 right-0 bg-white rounded-md shadow-lg">
+              <button
+                onClick={logoutHandler}
+                className=" px-4 py-2 hover:bg-gray-100"
+              >
+                Logout
+              </button>
+            </div>
           )}
         </div>
       </header>
